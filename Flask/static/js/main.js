caa60eadb983d7002dbbc6f25c898f21d369fa95
pavithram08 <124552975+pavithram08@users.noreply.github.com>
2023-04-20 17:30:55 +0530

    Update main.js

--- Flask/static/js/main.js
+++ Flask/static/js/main.js
@@ -22,15 +22,11 @@ function getPredictions(text) {
             var dl_pred = res.dl_pred;
             let outputMsg = "";
 
-            if (ml_pred == 1 && dl_pred == 1) {
+            if (ml_pred == "1") {
                 outputMsg = "Message is a Terrorism ideation";
-            } else if (ml_pred == 0 && dl_pred == 0) {
+            } else if (ml_pred == "0") {
                 outputMsg = "Message is not realted to terrorism ideation";
-            } else if (
-                (ml_pred == 0 && dl_pred == 1) ||
-                (ml_pred == 1 && dl_pred == 0)
-            ) {
-                outputMsg = "Message may be or may not be related terrorism ideation";
+            
             } else {
                 outputMsg = "Sorry,can't figure out!";
             }
